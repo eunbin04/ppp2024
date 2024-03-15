2c3d627a4a8c1707c643195cc49c23a387bc0880
eunbin04 <153293377+eunbin04@users.noreply.github.com>
2024-03-15 12:30:56 +0000

    2주차 과제 코드

--- hw03/q5_calorie_input.py
+++ hw03/q5_calorie_input.py
@@ -1,11 +1,14 @@
 hanlabog=100
 strawberry=100
 banana=100
+mango=100
 
 hanlabog=int(input("한라봉의 섭취량을 입력하시오."))
 strawberry=int(input("딸기의 섭취량을 입력하시오."))
 banana=int(input("바나나의 섭취량을 입력하시오."))
+mango=int(input("망고의 섭취량을 입력하시오."))
 hanlabog_calorie=(50/100*hanlabog)
 strawberry_calorie=(34/100*strawberry)
 banana_calorie=(77/100*banana)
-print("한라봉의 칼로리는 {}㎉, 딸기의 칼로리는 {}㎉, 바나나의 칼로리는 {}㎉ 입니다.".format(hanlabog_calorie, strawberry_calorie, banana_calorie))
+mango_calorie=(61/100*mango)
+print("한라봉의 칼로리는 {}㎉, 딸기의 칼로리는 {}㎉, 바나나의 칼로리는 {}㎉, 망고의 칼로리는 {}㎉ 입니다.".format(hanlabog_calorie, strawberry_calorie, banana_calorie, mango_calorie))
--- hw03/q6_x2y.py
+++ hw03/q6_x2y.py
@@ -1,9 +1,13 @@
-upper_side=3
-bottom_side=5
-height=4
+x1=5
+y1=4
+x2=1
+y2=1
 
-upper_side=int(input("윗변의 길이를 입력하시오."))
-bottom_side=int(input("밑변의 길이를 입력하시오."))
-height=int(input("높이를 입력하시오."))
-area=((upper_side+bottom_side)/2*height)
-print("사다리꼴의 면적={}".format(area))
+x1=int(input("A의 x축 좌표를 입력하시오."))
+y1=int(input("A의 y축 좌표를 입력하시오."))
+x2=int(input("B의 x축 좌표를 입력하시오."))
+y2=int(input("B의 y축 좌표를 입력하시오."))
+
+import math
+distance=(math.sqrt((x1-x2)**2+(y1-y2)**2))
+print("두 지점 A, B 사이의 거리=>{}".format(distance))
