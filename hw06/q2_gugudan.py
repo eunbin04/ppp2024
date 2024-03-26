6c0bec2bff483dff970c84899bc8e40c86485c9e
eunbin04 <153293377+eunbin04@users.noreply.github.com>
2024-03-26 04:05:50 +0000

    4주차 수업 코드

---
+++ hw06/q2_gugudan.py
@@ -0,0 +1,8 @@
+def main():
+    dan=int(input("구구단을 출력할 숫자를 입력하시오:"))
+
+    for i in range(9):
+        print(f"{dan}*{i+1}={dan*(i+1)}")
+
+    if __name__ == "__main__":
+        main()
--- lec05/def_ex.py
+++ lec05/def_ex.py
@@ -1,11 +1,9 @@
 def colored(r, g, b, text):
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
 
+print(colored(0, 255, 0, 'Hello, World'))
+print(colored(0, 0, 255, '모르겠어'))
+
 def hello(name):
     print(f"{colored(255, 0, 0, name)}님 안녕하세요.".format(name))
-
-
-print(colored(0, 255, 0, 'Hello, World'))
 hello("홍길동")
-
-#에러
