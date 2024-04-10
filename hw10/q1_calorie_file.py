22d0e1bb6b6abd0b2405a87f4cf10a57fa3e8a67
eunbin04 <153293377+eunbin04@users.noreply.github.com>
2024-04-10 08:55:33 +0000

    6주차 과제 코드

---
+++ hw10/q1_calorie_file.py
@@ -0,0 +1,24 @@
+def total_calorie(fruits_eat, fruits_cal_dic):
+    total = 0
+    for fruit_name in fruits_eat:
+        total += fruits_eat[fruit_name] * fruits_cal_dic[fruit_name] / 100
+    return total
+
+def read_cal_db(filename):
+    database={}
+    with open(filename, encoding="utf-8-sig") as f:
+        lines=f.readlines()
+        for line in lines[1:]:
+            tokens=line.split(",")
+            fruit_name=tokens[0]
+            fruit_cal=int(tokens[1])
+            database[fruit_name]=fruit_cal
+    return database
+
+def main():
+    fruits_calorie_dic=read_cal_db("hw10/my_fruit_calorie.csv")
+    fruits_yummy = {"딸기": 300, "고구마": 150}
+    print(total_calorie(fruits_yummy, fruits_calorie_dic))
+
+if __name__ == "__main__":
+    main()
