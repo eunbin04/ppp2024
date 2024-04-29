24e1432540db12633b2972bc99a2b0429cbdbd21
eunbin04 <153293377+eunbin04@users.noreply.github.com>
2024-04-29 11:29:39 +0000

    9주차 과제 코드

--- hw14/hw_weather_temp.py
+++ hw14/hw_weather_temp.py
@@ -1,29 +1,27 @@
 import sys
+import os
+import requests
 if "./" not in sys.path:
     sys.path.append("./")
-
 from lec13 import hw_submission
 
 def download(filename, url):
-    import os
-    import requests
-
     if not os.path.exists(filename):
         with open(filename,"w") as f:
             res =requests.get(url)
             f.write(res.text)
 
-from lec11.weather_q78 import read_col, read_col_int
+from weather_q78 import read_col, read_col_int
 
 def main():
     URL="https://data.kma.go.kr/stcs/grnd/downloadGrndTaList.do?fileType=csv&pgmNo=70&menuNo=432&serviceSe=F00101&stdrMg=99999&startDt=19040101&endDt=20240422&taElement=MIN&taElement=AVG&taElement=MAX&stnGroupSns=&selectType=1&mddlClssCd=SFC01&dataFormCd=F00501&dataTypeCd=standard&startDay=19040101&startYear=1904&endDay=20240422&endYear=2024&startMonth=01&endMonth=12&sesnCd=0&txtStnNm=%EC%A0%84%EC%A3%BC&stnId=146&areaId=&gFontSize="
     filename="./hw14/jeonju_all.csv"
     download(filename, URL)
 
-    tmax=read_col(filename, "Col 5")
-    tmin=read_col(filename, "Col 4")
-    tmax_date=read_col_int(filename, "Col 1")
-    tdiff_max_date=read_col_int(filename, "Col 1")
+    tmax=read_col(filename, "tmax")
+    tmin=read_col(filename, "tmin")
+    tmax_date=read_col_int(filename, "date")
+    tdiff_max_date=read_col_int(filename, "date")
 
     temp_diff=[]
     for tx, tn in zip(tmax, tmin):
@@ -37,11 +35,12 @@ def main():
             max_diff_temp=td
             max_idx=i
         i+=1
-
+    tdiff_max=max(temp_diff)
+    
     #tmax, tmax_date=38,"2021-08-12"
     #tdiff_max, tdiff_max_date=28.5, "1989-01-09"
     #hw_submission.submit("박은빈", tmax, tmax_date, tdiff_max, tdiff_max_date)
-    tdiff_max=max(temp_diff)
+    
     print(f"{max(tmax):.2f}℃ , {tmax_date[max_idx]} {tdiff_max:.2f}℃ , {tdiff_max_date[max_idx]}")
 
 if __name__=="__main__":
