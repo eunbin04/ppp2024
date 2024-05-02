0f11fe5c0bfc0b91714070d2e4b5bb1bbb0ceaab
eunbin04 <153293377+eunbin04@users.noreply.github.com>
2024-05-02 06:14:42 +0000

    테스트 파일

--- test_file/weather_read.py
+++ test_file/weather_read.py
@@ -1,3 +1,5 @@
+#header 위치 확인하는 법
+
 def read_col(filename, col_name):
     dataset=[]
     with open(filename) as f:
@@ -8,17 +10,6 @@ def read_col(filename, col_name):
         #     tokens=line.split(",")
         #     dataset.append(float(tokens[col_idx]))
     print(header)
-
-# def read_col_str(filename, col_name):
-#     dataset=[]
-#     with open(filename) as f:
-#         lines=f.readlines()
-#         header=[x.strip() for x in lines[7].split(",")]
-#         col_idx=header.index(col_name)
-#         for line in lines[8:]:
-#             tokens=line.split(",")
-#             dataset.append(str(tokens[col_idx]))
-#     return dataset
 
 def main():
     filename="./hw14/jeonju_all.csv"
